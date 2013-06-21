d53268198c9e09632b64c5944e6cbc7d41ddc9df
Madhes Waran <madhu@datareticle.com>
2013-06-21 18:38:01 +0530

    Update custom.js

--- custom.js
+++ custom.js
@@ -6,7 +6,7 @@
  * To change this template use File | Settings | File Templates.
  */
 window.onload=function(){
-    document.getElementsByTagName('body').innerHTML="my world";
+    document.getElementsByTagName('body').innerHTML="my world world";
 }
 
 
